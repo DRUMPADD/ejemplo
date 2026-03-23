516246e62ce802d5ad0e5a2ff1454aac579b9209
Erick Patiño <erickfco1999@gmail.com>
2026-03-23 12:38:34 -0600

    Aplicación

---
+++ take_pills_app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>Aqui va el menú (botón de menú para agregar medicamentos)</div>
+      <h1>Recordatorio de toma de pastillas</h1>
+      <section className='center recordatorio'>
+        <div>
+          Listado de los medicamentos y horarios para tomar pastillas.
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
